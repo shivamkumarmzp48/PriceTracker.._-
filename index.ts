707c80a9f854ee875dec4c54b71f0158d3259a44
shivamkumarmzp48 <124891656+shivamkumarmzp48@users.noreply.github.com>
2024-06-09 13:54:29 +0530

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,107 +1,85 @@
 "use server"
 
-import { EmailContent, EmailProductInfo, NotificationType } from '@/types';
-import nodemailer from 'nodemailer';
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import { extractCurrency, extractDescription, extractPrice } from '../utils';
 
-const Notification = {
-  WELCOME: 'WELCOME',
-  CHANGE_OF_STOCK: 'CHANGE_OF_STOCK',
-  LOWEST_PRICE: 'LOWEST_PRICE',
-  THRESHOLD_MET: 'THRESHOLD_MET',
-}
+export async function scrapeAmazonProduct(url: string) {
+  if(!url) return;
 
-export async function generateEmailBody(
-  product: EmailProductInfo,
-  type: NotificationType
-  ) {
-  const THRESHOLD_PERCENTAGE = 40;
-  // Shorten the product title
-  const shortenedTitle =
-    product.title.length > 20
-      ? `${product.title.substring(0, 20)}...`
-      : product.title;
+  // BrightData proxy configuration
+  const username = String(process.env.BRIGHT_DATA_USERNAME);
+  const password = String(process.env.BRIGHT_DATA_PASSWORD);
+  const port = 22225;
+  const session_id = (1000000 * Math.random()) | 0;
 
-  let subject = "";
-  let body = "";
-
-  switch (type) {
-    case Notification.WELCOME:
-      subject = `Welcome to Price Tracking for ${shortenedTitle}`;
-      body = `
-        <div>
-          <h2>Welcome to PriceWise 🚀</h2>
-          <p>You are now tracking ${product.title}.</p>
-          <p>Here's an example of how you'll receive updates:</p>
-          <div style="border: 1px solid #ccc; padding: 10px; background-color: #f8f8f8;">
-            <h3>${product.title} is back in stock!</h3>
-            <p>We're excited to let you know that ${product.title} is now back in stock.</p>
-            <p>Don't miss out - <a href="${product.url}" target="_blank" rel="noopener noreferrer">buy it now</a>!</p>
-            <img src="https://i.ibb.co/pwFBRMC/Screenshot-2023-09-26-at-1-47-50-AM.png" alt="Product Image" style="max-width: 100%;" />
-          </div>
-          <p>Stay tuned for more updates on ${product.title} and other products you're tracking.</p>
-        </div>
-      `;
-      break;
-
-    case Notification.CHANGE_OF_STOCK:
-      subject = `${shortenedTitle} is now back in stock!`;
-      body = `
-        <div>
-          <h4>Hey, ${product.title} is now restocked! Grab yours before they run out again!</h4>
-          <p>See the product <a href="${product.url}" target="_blank" rel="noopener noreferrer">here</a>.</p>
-        </div>
-      `;
-      break;
-
-    case Notification.LOWEST_PRICE:
-      subject = `Lowest Price Alert for ${shortenedTitle}`;
-      body = `
-        <div>
-          <h4>Hey, ${product.title} has reached its lowest price ever!!</h4>
-          <p>Grab the product <a href="${product.url}" target="_blank" rel="noopener noreferrer">here</a> now.</p>
-        </div>
-      `;
-      break;
-
-    case Notification.THRESHOLD_MET:
-      subject = `Discount Alert for ${shortenedTitle}`;
-      body = `
-        <div>
-          <h4>Hey, ${product.title} is now available at a discount more than ${THRESHOLD_PERCENTAGE}%!</h4>
-          <p>Grab it right away from <a href="${product.url}" target="_blank" rel="noopener noreferrer">here</a>.</p>
-        </div>
-      `;
-      break;
-
-    default:
-      throw new Error("Invalid notification type.");
+  const options = {
+    auth: {
+      username: `${username}-session-${session_id}`,
+      password,
+    },
+    host: 'brd.superproxy.io',
+    port,
+    rejectUnauthorized: false,
   }
 
-  return { subject, body };
-}
+  try {
+    // Fetch the product page
+    const response = await axios.get(url, options);
+    const $ = cheerio.load(response.data);
 
-const transporter = nodemailer.createTransport({
-  pool: true,
-  service: 'hotmail',
-  port: 2525,
-  auth: {
-    user: 'javascriptmastery@outlook.com',
-    pass: process.env.EMAIL_PASSWORD,
-  },
-  maxConnections: 1
-})
+    // Extract the product title
+    const title = $('#productTitle').text().trim();
+    const currentPrice = extractPrice(
+      $('.priceToPay span.a-price-whole'),
+      $('.a.size.base.a-color-price'),
+      $('.a-button-selected .a-color-base'),
+    );
 
-export const sendEmail = async (emailContent: EmailContent, sendTo: string[]) => {
-  const mailOptions = {
-    from: 'javascriptmastery@outlook.com',
-    to: sendTo,
-    html: emailContent.body,
-    subject: emailContent.subject,
+    const originalPrice = extractPrice(
+      $('#priceblock_ourprice'),
+      $('.a-price.a-text-price span.a-offscreen'),
+      $('#listPrice'),
+      $('#priceblock_dealprice'),
+      $('.a-size-base.a-color-price')
+    );
+
+    const outOfStock = $('#availability span').text().trim().toLowerCase() === 'currently unavailable';
+
+    const images = 
+      $('#imgBlkFront').attr('data-a-dynamic-image') || 
+      $('#landingImage').attr('data-a-dynamic-image') ||
+      '{}'
+
+    const imageUrls = Object.keys(JSON.parse(images));
+
+    const currency = extractCurrency($('.a-price-symbol'))
+    const discountRate = $('.savingsPercentage').text().replace(/[-%]/g, "");
+
+    const description = extractDescription($)
+
+    // Construct data object with scraped information
+    const data = {
+      url,
+      currency: currency || '$',
+      image: imageUrls[0],
+      title,
+      currentPrice: Number(currentPrice) || Number(originalPrice),
+      originalPrice: Number(originalPrice) || Number(currentPrice),
+      priceHistory: [],
+      discountRate: Number(discountRate),
+      category: 'category',
+      reviewsCount:100,
+      stars: 4.5,
+      isOutOfStock: outOfStock,
+      description,
+      lowestPrice: Number(currentPrice) || Number(originalPrice),
+      highestPrice: Number(originalPrice) || Number(currentPrice),
+      averagePrice: Number(currentPrice) || Number(originalPrice),
+    }
+
+    return data;
+  } catch (error: any) {
+    console.log(error);
   }
-
-  transporter.sendMail(mailOptions, (error: any, info: any) => {
-    if(error) return console.log(error);
-    
-    console.log('Email sent: ', info);
-  })
 }
