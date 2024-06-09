7c15dd50b44ccb91938b212707dda53e89370ba1
shivamkumarmzp48 <124891656+shivamkumarmzp48@users.noreply.github.com>
2024-06-09 13:53:50 +0530

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,116 +1,107 @@
 "use server"
 
-import { revalidatePath } from "next/cache";
-import Product from "../models/product.model";
-import { connectToDB } from "../mongoose";
-import { scrapeAmazonProduct } from "../scraper";
-import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
-import { User } from "@/types";
-import { generateEmailBody, sendEmail } from "../nodemailer";
+import { EmailContent, EmailProductInfo, NotificationType } from '@/types';
+import nodemailer from 'nodemailer';
 
-export async function scrapeAndStoreProduct(productUrl: string) {
-  if(!productUrl) return;
-
-  try {
-    connectToDB();
-
-    const scrapedProduct = await scrapeAmazonProduct(productUrl);
-
-    if(!scrapedProduct) return;
-
-    let product = scrapedProduct;
-
-    const existingProduct = await Product.findOne({ url: scrapedProduct.url });
-
-    if(existingProduct) {
-      const updatedPriceHistory: any = [
-        ...existingProduct.priceHistory,
-        { price: scrapedProduct.currentPrice }
-      ]
-
-      product = {
-        ...scrapedProduct,
-        priceHistory: updatedPriceHistory,
-        lowestPrice: getLowestPrice(updatedPriceHistory),
-        highestPrice: getHighestPrice(updatedPriceHistory),
-        averagePrice: getAveragePrice(updatedPriceHistory),
-      }
-    }
-
-    const newProduct = await Product.findOneAndUpdate(
-      { url: scrapedProduct.url },
-      product,
-      { upsert: true, new: true }
-    );
-
-    revalidatePath(`/products/${newProduct._id}`);
-  } catch (error: any) {
-    throw new Error(`Failed to create/update product: ${error.message}`)
-  }
+const Notification = {
+  WELCOME: 'WELCOME',
+  CHANGE_OF_STOCK: 'CHANGE_OF_STOCK',
+  LOWEST_PRICE: 'LOWEST_PRICE',
+  THRESHOLD_MET: 'THRESHOLD_MET',
 }
 
-export async function getProductById(productId: string) {
-  try {
-    connectToDB();
+export async function generateEmailBody(
+  product: EmailProductInfo,
+  type: NotificationType
+  ) {
+  const THRESHOLD_PERCENTAGE = 40;
+  // Shorten the product title
+  const shortenedTitle =
+    product.title.length > 20
+      ? `${product.title.substring(0, 20)}...`
+      : product.title;
 
-    const product = await Product.findOne({ _id: productId });
+  let subject = "";
+  let body = "";
 
-    if(!product) return null;
+  switch (type) {
+    case Notification.WELCOME:
+      subject = `Welcome to Price Tracking for ${shortenedTitle}`;
+      body = `
+        <div>
+          <h2>Welcome to PriceWise 🚀</h2>
+          <p>You are now tracking ${product.title}.</p>
+          <p>Here's an example of how you'll receive updates:</p>
+          <div style="border: 1px solid #ccc; padding: 10px; background-color: #f8f8f8;">
+            <h3>${product.title} is back in stock!</h3>
+            <p>We're excited to let you know that ${product.title} is now back in stock.</p>
+            <p>Don't miss out - <a href="${product.url}" target="_blank" rel="noopener noreferrer">buy it now</a>!</p>
+            <img src="https://i.ibb.co/pwFBRMC/Screenshot-2023-09-26-at-1-47-50-AM.png" alt="Product Image" style="max-width: 100%;" />
+          </div>
+          <p>Stay tuned for more updates on ${product.title} and other products you're tracking.</p>
+        </div>
+      `;
+      break;
 
-    return product;
-  } catch (error) {
-    console.log(error);
+    case Notification.CHANGE_OF_STOCK:
+      subject = `${shortenedTitle} is now back in stock!`;
+      body = `
+        <div>
+          <h4>Hey, ${product.title} is now restocked! Grab yours before they run out again!</h4>
+          <p>See the product <a href="${product.url}" target="_blank" rel="noopener noreferrer">here</a>.</p>
+        </div>
+      `;
+      break;
+
+    case Notification.LOWEST_PRICE:
+      subject = `Lowest Price Alert for ${shortenedTitle}`;
+      body = `
+        <div>
+          <h4>Hey, ${product.title} has reached its lowest price ever!!</h4>
+          <p>Grab the product <a href="${product.url}" target="_blank" rel="noopener noreferrer">here</a> now.</p>
+        </div>
+      `;
+      break;
+
+    case Notification.THRESHOLD_MET:
+      subject = `Discount Alert for ${shortenedTitle}`;
+      body = `
+        <div>
+          <h4>Hey, ${product.title} is now available at a discount more than ${THRESHOLD_PERCENTAGE}%!</h4>
+          <p>Grab it right away from <a href="${product.url}" target="_blank" rel="noopener noreferrer">here</a>.</p>
+        </div>
+      `;
+      break;
+
+    default:
+      throw new Error("Invalid notification type.");
   }
+
+  return { subject, body };
 }
 
-export async function getAllProducts() {
-  try {
-    connectToDB();
+const transporter = nodemailer.createTransport({
+  pool: true,
+  service: 'hotmail',
+  port: 2525,
+  auth: {
+    user: 'javascriptmastery@outlook.com',
+    pass: process.env.EMAIL_PASSWORD,
+  },
+  maxConnections: 1
+})
 
-    const products = await Product.find();
-
-    return products;
-  } catch (error) {
-    console.log(error);
+export const sendEmail = async (emailContent: EmailContent, sendTo: string[]) => {
+  const mailOptions = {
+    from: 'javascriptmastery@outlook.com',
+    to: sendTo,
+    html: emailContent.body,
+    subject: emailContent.subject,
   }
-}
 
-export async function getSimilarProducts(productId: string) {
-  try {
-    connectToDB();
-
-    const currentProduct = await Product.findById(productId);
-
-    if(!currentProduct) return null;
-
-    const similarProducts = await Product.find({
-      _id: { $ne: productId },
-    }).limit(3);
-
-    return similarProducts;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-export async function addUserEmailToProduct(productId: string, userEmail: string) {
-  try {
-    const product = await Product.findById(productId);
-
-    if(!product) return;
-
-    const userExists = product.users.some((user: User) => user.email === userEmail);
-
-    if(!userExists) {
-      product.users.push({ email: userEmail });
-
-      await product.save();
-
-      const emailContent = await generateEmailBody(product, "WELCOME");
-
-      await sendEmail(emailContent, [userEmail]);
-    }
-  } catch (error) {
-    console.log(error);
-  }
+  transporter.sendMail(mailOptions, (error: any, info: any) => {
+    if(error) return console.log(error);
+    
+    console.log('Email sent: ', info);
+  })
 }
